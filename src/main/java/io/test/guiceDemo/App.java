a211061d8d9f4f2c50e799f5e74745d4e549986f
babyplus <maxcrazy@yeah.net>
2018-02-05 23:34:08 +0800

    new file:   .gitignore 	new file:   pom.xml 	new file:   src/main/java/io/test/guiceDemo/App.java 	new file:   src/main/java/io/test/guiceDemo/helloWorldDemo/HelloWorldPrinter.java 	new file:   src/test/java/io/test/guiceDemo/AppTest.java
    	eclipse 快捷键 "ctrl + 1" "shift + alt + r"

---
+++ src/main/java/io/test/guiceDemo/App.java
@@ -0,0 +1,15 @@
+package io.test.guiceDemo;
+
+import io.test.guiceDemo.helloWorldDemo.HelloWorldPrinter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		new HelloWorldPrinter().printHelloWorld();
+	}
+
+
+}
---
+++ src/main/java/io/test/guiceDemo/helloWorldDemo/HelloWorldPrinter.java
@@ -0,0 +1,9 @@
+package io.test.guiceDemo.helloWorldDemo;
+
+public class HelloWorldPrinter {
+
+	public void printHelloWorld() {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+}
