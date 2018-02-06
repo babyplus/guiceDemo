6889640fee742b83f0f4cf2dd7d39c69532b3693
babyplus <maxcrazy@yeah.net>
2018-02-06 23:25:11 +0800

    modified:   src/main/java/io/test/guiceDemo/Configuration.java 	renamed:    src/main/java/io/test/guiceDemo/helloWorldDemo/HelloWorldPrinter.java -> src/main/java/io/test/guiceDemo/helloWorldDemo/StringWritingApplet.java
    	修改类名和方法名

--- src/main/java/io/test/guiceDemo/Configuration.java
+++ src/main/java/io/test/guiceDemo/Configuration.java
@@ -1,13 +1,13 @@
 package io.test.guiceDemo;
 
-import io.test.guiceDemo.helloWorldDemo.HelloWorldPrinter;
+import io.test.guiceDemo.helloWorldDemo.StringWritingApplet;
 import io.test.guiceDemo.helloWorldDemo.PrintStreamWriter;
 
 public class Configuration {
 
 	public static MyApplet getMainApplet() {
 		// TODO Auto-generated method stub
-		return new HelloWorldPrinter(new PrintStreamWriter(System.out), "helloConfiguration!");
+		return new StringWritingApplet(new PrintStreamWriter(System.out), "helloConfiguration!");
 	}
 
 }
--- src/main/java/io/test/guiceDemo/helloWorldDemo/StringWritingApplet.java
+++ src/main/java/io/test/guiceDemo/helloWorldDemo/StringWritingApplet.java
@@ -2,23 +2,23 @@ package io.test.guiceDemo.helloWorldDemo;
 
 import io.test.guiceDemo.MyApplet;
 
-public class HelloWorldPrinter implements MyApplet {
+public class StringWritingApplet implements MyApplet {
 
 	private MyDestination destination;
 	private String string;
 
-	public HelloWorldPrinter(MyDestination destination, String string) {
+	public StringWritingApplet(MyDestination destination, String string) {
 		super();
 		this.destination = destination;
 		this.string = string;
 	}
 
-	private void printHelloWorld() {
+	private void printString() {
 		destination.write(string);
 	}
 
 	public void run() {
 		// TODO Auto-generated method stub
-		printHelloWorld();
+		printString();
 	}
 }
